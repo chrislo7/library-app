58fc79d91344a92727afb43de89db468230b438a
Chris <chrislo.7@live.com>
2020-08-16 00:58:01 -0400

    added pagination and search

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,3 +132,8 @@ FIXTURE_DIRS = (
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
+}
--- backend/library/views.py
+++ backend/library/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination, filters
 from .serializers import BookSerializer
 from .models import Book 
 
 class BookView(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    pagination_class = pagination.PageNumberPagination
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ['title']

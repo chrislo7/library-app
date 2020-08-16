1d179762881a1233c2cd5231d7f8175f3500ef8d
Chris <chrislo.7@live.com>
2020-08-16 02:42:50 -0400

    using axios to grab actual data; next/previous page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,112 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
 
 // hardcoded values
-
-const response = {
-    "count": 10,
-    "next": "http://localhost:8000/api/books/?page=2",
-    "previous": null,
-    "results": [
-        {
-            "id": "1",
-            "title": "Python Crash Course",
-            "author": "Eric Matthes",
-            "quantity": 5,
-            "reserved": false
-        },
-        {
-            "id": "2",
-            "title": "Head-First Python",
-            "author": "Paul Barry",
-            "quantity": 2,
-            "reserved": false
-        },
-        {
-            "id": "3",
-            "title": "Invent Your Own Computer Games with Python",
-            "author": "Al Sweigart",
-            "quantity": 1,
-            "reserved": true
-        }
-    ]
-};
+// const response = {
+//     "count": 10,
+//     "next": "http://localhost:8000/api/books/?page=2",
+//     "previous": null,
+//     "results": [
+//         {
+//             "id": "1",
+//             "title": "Python Crash Course",
+//             "author": "Eric Matthes",
+//             "quantity": 5,
+//             "reserved": false
+//         },
+//         {
+//             "id": "2",
+//             "title": "Head-First Python",
+//             "author": "Paul Barry",
+//             "quantity": 2,
+//             "reserved": false
+//         },
+//         {
+//             "id": "3",
+//             "title": "Invent Your Own Computer Games with Python",
+//             "author": "Al Sweigart",
+//             "quantity": 1,
+//             "reserved": true
+//         }
+//     ]
+// };
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             viewReserved: false,
-            bookList: response.results,
-            nextPage: response.next,
-            previousPage: response.previous
+            bookList: [],
+            nextPage: null,
+            previousPage: null
         };
-    }
+    };
+
+    componentDidMount() {
+        this.refreshList();        
+    };
+
+    // handleChange = (e) => {
+    //     this.setState({ input: e.target.value });
+    // };
+
+    refreshList = () => {
+        axios
+            .get("http://localhost:8000/api/books/")
+            .then((res) => {
+                console.log(res.data);
+                this.setState({ bookList: res.data.results })
+                this.setState({ nextPage: res.data.next })
+                this.setState({ previousPage: res.data.previous })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    };
+
+    search = (query) => {
+        axios
+            .get("http://localhost:8000/api/books/?search=" + query)
+            .then((res) => {
+                this.setState({ bookList: res.data.results })
+                this.setState({ nextPage: res.data.next })
+                this.setState({ previousPage: res.data.previous })
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    };
+
+    changePage = (page) => {
+        // page === 'next'
+        // nextPage is the url
+        // 
+
+        if (page === 'next') {
+            axios
+                .get(this.state.nextPage)
+                .then((res) => {
+                    this.setState({ bookList: res.data.results })
+                    this.setState({ nextPage: res.data.next })
+                    this.setState({ previousPage: res.data.previous })
+                })
+                .catch((err) => {
+                    console.log(err)
+                });
+        } else {
+            axios
+                .get(this.state.previousPage)
+                .then((res) => {
+                    this.setState({ bookList: res.data.results })
+                    this.setState({ nextPage: res.data.next })
+                    this.setState({ previousPage: res.data.previous })
+                })
+                .catch((err) => {
+                    console.log(err)
+                });        
+        }
+    };
 
     displayReserved = (status) => {
         if (status) {
@@ -110,12 +175,16 @@ class App extends Component {
     renderPageTabs = () => {
         return (
             <div className="page-btn-container">
-                <form className="page-btn" action={`${this.state.previousPage}`}>
-                    <input className="btn-input" type="submit" value="Previous" />
-                </form>
-                <form className="page-btn" action={`${this.state.nextPage}`}>
-                    <input className="btn-input" type="submit" value="Next" />
-                </form>
+                {this.state.previousPage &&
+                    <button className="page-btn" onClick={() => this.changePage('previous')}>
+                        Previous Page
+                    </button>
+                }
+                {this.state.nextPage &&
+                    <button className="page-btn" onClick={() => this.changePage('next')}>
+                        Next Page
+                    </button>
+                }
             </div>
         );
     };
